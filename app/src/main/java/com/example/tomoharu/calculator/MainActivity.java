d4fa0350b904c73a906555f6b3ed572f82ede1b0
t-nagashima <t.nagashima.pm@gmail.com>
2015-03-08 23:44:53 +0900

    First app

--- app/src/main/java/com/example/tomoharu/calculator/MainActivity.java
+++ app/src/main/java/com/example/tomoharu/calculator/MainActivity.java
@@ -1,19 +1,39 @@
 package com.example.tomoharu.calculator;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    String strTemp="";
+    String strResult="0";
+    int operator=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showNumber(strTemp);
+        readPreferences();
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        writePreferences();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +55,130 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void numKeyOnClick(View v){
+        String strInKey = (String) ((Button)v).getText();
+
+        if(strInKey.equals(".")){
+            if(strTemp.length()==0){
+                strTemp="0.";
+            }else{
+                if(strTemp.indexOf(".")==-1){
+                    strTemp=strTemp+".";
+                }
+            }
+        }else{
+            strTemp=strTemp+strInKey;
+        }
+
+        showNumber(strTemp);
+    }
+
+    public void functionKeyOnClick(View v){
+        switch(v.getId()){
+            case R.id.BtAC:
+                strTemp="";
+                strResult="0";
+                operator = 0;
+                break;
+            case R.id.BtC:
+                strTemp="";
+                break;
+        }
+
+        showNumber(strTemp);
+    }
+
+    public void operatorKeyOnClick(View v){
+        if(operator!=0){
+            if(strTemp.length()>0){
+                strResult = doCalc();
+                showNumber(strResult);
+            }
+        }else{
+            if(strTemp.length()>0){
+                strResult=strTemp;
+            }
+        }
+
+        strTemp = "";
+
+        if(v.getId()==R.id.BtEq){
+            operator = 0;
+        }else{
+            operator = v.getId();
+        }
+
+    }
+
+    private void showNumber(String strNum){
+        DecimalFormat form = new DecimalFormat("#,##0");
+        String strDecimal="";
+        String strInt="";
+        String fText="";
+
+        if(strNum.length() > 0){
+            int decimalPoint = strNum.indexOf(".");
+            if(decimalPoint>-1){
+                strDecimal = strNum.substring(decimalPoint);
+                strInt = strNum.substring(0, decimalPoint);
+            }else{
+                strInt = strNum;
+            }
+            fText = form.format(Double.parseDouble(strInt))+strDecimal;
+        }else fText = "0";
+
+        ((TextView)findViewById(R.id.displayPanel)).setText(fText);
+
+    }
+
+    private String doCalc(){
+        BigDecimal bd1 = new BigDecimal(strResult);
+        BigDecimal bd2 = new BigDecimal(strTemp);
+        BigDecimal result = BigDecimal.ZERO;
+
+        switch(operator){
+            case R.id.BtPlu:
+                result = bd1.add(bd2);
+                break;
+            case R.id.BtMin:
+                result = bd1.subtract(bd2);
+                break;
+            case R.id.BtMul:
+                result = bd1.multiply(bd2);
+                break;
+            case R.id.BtDiv:
+                if(!bd2.equals(BigDecimal.ZERO)) {
+                    result = bd1.divide(bd2, 12, 3);
+                }else{
+                    Toast toast = Toast.makeText(this, "Don't divide by zero!", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+                break;
+        }
+
+        if(result.toString().indexOf(".")>=0){
+            return result.toString().replaceAll("\\.0+$|0+$", "");
+        }else{
+            return result.toString();
+        }
+    }
+
+    private void writePreferences(){
+        SharedPreferences prefs = getSharedPreferences("CalcPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("strTemp", strTemp);
+        editor.putString("strResult", strResult);
+        editor.putInt("operator", operator);
+        editor.putString("strDisplay", ((TextView)findViewById(R.id.displayPanel)).getText().toString());
+    }
+
+    private void readPreferences(){
+        SharedPreferences prefs = getSharedPreferences("CalcPrefs", MODE_PRIVATE);
+        strTemp = prefs.getString("strTemp", "");
+        strResult = prefs.getString("strResult", "0");
+        operator = prefs.getInt("operator", 0);
+        ((TextView)findViewById(R.id.displayPanel)).setText(prefs.getString("strDisplay", "0"));
     }
 }
